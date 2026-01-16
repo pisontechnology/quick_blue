9d635e2f57746c5219fafeaab22f19585ce5edd0
Nicholas Craffey <nick.craffey@pison.com>
2026-01-16 13:45:35 -0500

    for testing

--- quick_blue_darwin/darwin/Classes/QuickBlueDarwinPlugin.swift
+++ quick_blue_darwin/darwin/Classes/QuickBlueDarwinPlugin.swift
@@ -355,8 +355,7 @@ extension QuickBlueDarwinPlugin: CBCentralManagerDelegate {
                 ?? []
             // Prioritize advertised local name over peripheral.name
             // peripheral.name can be nil or stale, but CBAdvertisementDataLocalNameKey contains the current advertised name
-//            let localName = advertisementData[CBAdvertisementDataLocalNameKey] as? String
-            let localName = "craffTest"
+            let localName = advertisementData[CBAdvertisementDataLocalNameKey] as? String
             let deviceName = localName ?? peripheral.name ?? ""
             if targetManufacturerData != nil {
                 if targetManufacturerData == manufacturerData {
